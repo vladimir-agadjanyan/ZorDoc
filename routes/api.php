292dce012adc71be3359706d2946e8b1f715507c
Vladimir <vladimir,agadjanyan@gmail.com>
2026-05-19 23:32:53 +0500

    *

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::post('/images', [ImageController::class, 'upload']);
+    Route::post('/images', [ImageController::class, 'upload'])->middleware('throttle:100000, 1440');
     Route::get('/images', [ImageController::class, 'index']);
     Route::get('/images/{image}', [ImageController::class, 'show']);
     Route::delete('/images/{image}', [ImageController::class, 'destroy']);
